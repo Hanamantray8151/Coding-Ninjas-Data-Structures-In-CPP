1373826f75957dfad2b76906abb1760be1d169cc
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-06-25 14:45:29 +0530

    Update factorial_recursion.cpp

--- Recursion1/factorial_recursion.cpp
+++ Recursion1/factorial_recursion.cpp
@@ -1,1 +1,19 @@
+#include<iostream>
+using namespace std;
 
+int fact(int n){
+    cout<<n<<endl;
+    if(n==0){
+        return 1;
+    }
+    int smalloutput = fact(n-1);
+    return n*smalloutput;
+}
+int main(){
+    int n;
+    cout<<"Enter Number To find Factorial"<<endl;
+    cin>>n;
+    int output = fact(n);
+    cout<<output<<endl;
+    return 0;
+}
