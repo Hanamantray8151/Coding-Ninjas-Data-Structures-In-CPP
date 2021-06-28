a79f99f3ac19b2df94a851d0b73f201b2a14d1ff
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-06-28 23:29:37 +0530

    Add files via upload

---
+++ Recursion2/mergesort_recursion.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int input[], int s, int mid, int e){
+    int temp[e - s + 1];
+    int i = s;
+    int j = mid + 1;
+    //Let one part of array is from s to mid and another from mid+1 to end
+    int k = 0;
+    
+    while(i<=mid && j<=e){
+        if(input[i]<=input[j]){
+            temp[k++] = input[i];
+            i++;
+        }
+        else{
+            temp[k++] = input[j];
+            j++;
+        }
+    }
+    while(i<=mid) {
+        temp[k++] = input[i];
+        i++;
+    }
+    while(j<=e) {
+        temp[k++] = input[j];
+        j++;
+    }
+    
+    for(int i=s;i<=e;i++) {
+        input[i] = temp[i-s];
+    }
+}
+
+void mergeSort(int input[],int si,int ei){
+	if(si >= ei){
+		return;
+	}
+	int mid = (si + ei)/2;
+	mergeSort(input,si,mid);
+	mergeSort(input,mid + 1,ei);
+	merge(input,si,mid,ei);
+}
+
+void mergeSort(int input[], int size){
+	if(size == 0 || size == 1){
+		return;
+	}
+	mergeSort(input,0,size-1);
+}
+
+int main(){
+	int size;
+	cin >> size;
+	int input[size];
+	for(int i=0;i<size;i++){
+		cin >> input[i];
+	}
+	mergeSort(input,size);
+	for(int i=0;i<size;i++){
+		cout << input[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
