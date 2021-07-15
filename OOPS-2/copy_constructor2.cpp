d0d37bbcf4c9d861b2dc2b73bce671b0c352a4d0
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-16 00:24:05 +0530

    Update copy_constructor2.cpp

--- OOPS-2/copy_constructor2.cpp
+++ OOPS-2/copy_constructor2.cpp
@@ -14,8 +14,11 @@ class Student{
 			this -> name = new char[strlen(name) + 1];
 			strcpy(this -> name,name);
 		}
-		//Student(Student s){	//If we write this,here it will go infinite loop bcz we need copy constructor and passing copy constructor as argument this is bad.
-		Student(Student const &s){	//if we write this,here s is reference to the s1 object,that means s and s1 are pointing towards same memory bloack.
+		//Student(Student s){	//If we write this,here it will go infinite loop bcz we need copy constructor and
+					//passing copy constructor as argument this is bad.
+	
+		Student(Student const &s){	//if we write this,here s is reference to the s1 object,that means s and s1 are pointing
+						//towards same memory bloack.
 			this -> age = s.age;
 			//this -> name = s.name;	Shallow Copy.
 			
