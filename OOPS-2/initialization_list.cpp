1d253b861e2c7410d8ece810e34c49f6be182964
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-16 13:00:03 +0530

    Update initialization_list.cpp

--- OOPS-2/initialization_list.cpp
+++ OOPS-2/initialization_list.cpp
@@ -6,8 +6,8 @@ class Student{
 		int age;
 		const int rno;	//Constant variable.
 		int &x;	//reference variable for age;
-	Student(int r,int age) : rno(r),age(age),x(this -> age){	//We must use initialization list to initialize constant variables at the time of declaration.
-                                                            //for Non constant variables it's optional.
+	Student(int r,int age) : rno(r),age(age),x(this -> age){  //We must use initialization list to initialize constant variables at the time of declaration.
+                                                            	  //for Non constant variables it's optional.
 	}
 	void print(){
 		cout << age << " " << rno << " " << x << endl;
