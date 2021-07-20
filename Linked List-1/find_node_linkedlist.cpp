db9e36e25d78b4377649686bc26f537f693af7be
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-21 01:14:49 +0530

    Update find_node_linkedlist.cpp

--- Linked List-1/find_node_linkedlist.cpp
+++ Linked List-1/find_node_linkedlist.cpp
@@ -2,54 +2,54 @@
 using namespace std;
 
 class Node{
-       public:
-	        int data;
+	public:
+		int data;
 	        Node *next;
 
 	        Node(int data){
-		          this->data = data;
-		          this->next = NULL;
+			this->data = data;
+		        this->next = NULL;
 	        }
 };
 
 Node *takeinput(){
-      int data;
-	    cin >> data;
-	    Node *head = NULL, *tail = NULL;
-	    while (data != -1){
-		      Node *newNode = new Node(data);
-		      if (head == NULL){
-			        head = newNode;
-			        tail = newNode;
-		      }else{
-			        tail->next = newNode;
-			        tail = newNode;
-		      }
-		      cin >> data;
-	    }
-	    return head;
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1){
+		Node *newNode = new Node(data);
+		if (head == NULL){
+			head = newNode;
+			tail = newNode;
+		}else{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;    	   	
 }
 
 int findNode(Node *head, int n){
-      Node *temp = head;
-      int count = 0;
-      while(temp != NULL){
-          if(temp -> data == n){
-              return count;
-          }
-          count++;
-          temp = temp -> next;
-      }
-      return -1;
+      	Node *temp = head;
+      	int count = 0;
+      	while(temp != NULL){
+          	if(temp -> data == n){
+              		return count;
+          	}
+          	count++;
+          	temp = temp -> next;
+      	}
+      	return -1;
 }
 
 int main(){
-      int t;
-	    cin >> t;
-	    while (t--){
-		      Node *head = takeinput();
-		      int val;
-		      cin >> val;
-		      cout << findNode(head, val) << endl;
-	    }
+      	int t;
+	cin >> t;
+	while (t--){
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNode(head, val) << endl;
+	}
 }
