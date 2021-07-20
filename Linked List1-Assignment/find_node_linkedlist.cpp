31a30b276cb9de22ac31f971446b950b8cfe1366
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-21 01:15:29 +0530

    Update find_node_linkedlist.cpp

--- Linked List1-Assignment/find_node_linkedlist.cpp
+++ Linked List1-Assignment/find_node_linkedlist.cpp
@@ -41,3 +41,58 @@ For the given singly linked list, considering the indices starting from 0, progr
 then the N = 6 appears at position 4.
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+	        Node *next;
+
+	        Node(int data){
+			this->data = data;
+		        this->next = NULL;
+	        }
+};
+
+Node *takeinput(){
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1){
+		Node *newNode = new Node(data);
+		if (head == NULL){
+			head = newNode;
+			tail = newNode;
+		}else{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin >> data;
+	}
+	return head;    	   	
+}
+
+int findNode(Node *head, int n){
+      	Node *temp = head;
+      	int count = 0;
+      	while(temp != NULL){
+          	if(temp -> data == n){
+              		return count;
+          	}
+          	count++;
+          	temp = temp -> next;
+      	}
+      	return -1;
+}
+
+int main(){
+      	int t;
+	cin >> t;
+	while (t--){
+		Node *head = takeinput();
+		int val;
+		cin >> val;
+		cout << findNode(head, val) << endl;
+	}
+}
