89903067778fd43a2649e33e9af9042ca9524b5c
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-25 00:05:01 +0530

    Update bubblesort_iterative_inll.cpp

--- Linked List2-Assignment/bubblesort_iterative_inll.cpp
+++ Linked List2-Assignment/bubblesort_iterative_inll.cpp
@@ -58,37 +58,37 @@ Node *takeinput(){
 }
 
 int length(node* head){
-    if(head==NULL){
-        return 0;
-    }
-    node* temp=head;
-    int size=1+length(temp->next);
-    return size;
+    	if(head==NULL){
+        	return 0;
+   	}
+    	node* temp=head;
+    	int size=1+length(temp->next);
+    	return size;
 }
 node* bubble_sort_LinkedList_itr(node* head){
-    for(int i=0;length(head)>i;i++){ 
-        node* prev=NULL,*curr=head;
-        while(curr->next!=NULL){
-            if(curr->data>curr->next->data){
-                if(prev!=NULL){
-                    node *temp = curr -> next -> next;
-                    curr -> next -> next = curr;
-                    prev -> next = curr -> next;
-                    curr -> next = temp;
-                    prev = prev -> next;
-                }else{
-                    head=curr->next;
-                    curr->next=head->next;
-                    head->next=curr;
-                    prev=head;
-                }
-            }else{ 
-                prev=curr;
-                curr=curr->next;
-            }
-        }
-    }
-    return head;
+    	for(int i=0;length(head)>i;i++){ 
+        	node* prev=NULL,*curr=head;
+        	while(curr->next!=NULL){
+            		if(curr->data>curr->next->data){
+                		if(prev!=NULL){
+                    			node *temp = curr -> next -> next;
+                    			curr -> next -> next = curr;
+                    			prev -> next = curr -> next;
+                    			curr -> next = temp;
+                    			prev = prev -> next;
+                		}else{
+                    		head=curr->next;
+                    		curr->next=head->next;
+                    		head->next=curr;
+                    		prev=head;
+                		}
+            		}else{ 
+                		prev=curr;
+                		curr=curr->next;
+            		}
+        	}
+    	}
+    	return head;
 }
 
 void print(Node *head){
