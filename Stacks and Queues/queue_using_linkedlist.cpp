cfa17d74027f5a2497e78ed2fb4f795799e3d7f5
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-28 18:46:00 +0530

    Update queue_using_linkedlist.cpp

--- Stacks and Queues/queue_using_linkedlist.cpp
+++ Stacks and Queues/queue_using_linkedlist.cpp
@@ -154,31 +154,31 @@ public:
 };
 
 int main() {
-    Queue q;
+    	Queue q;
 
-    int t;
-    cin >> t;
+   	int t;
+    	cin >> t;
 
-    while (t--) {
-        int choice, input;
-        cin >> choice;
-        switch (choice) {
-            case 1:
-                cin >> input;
-                q.enqueue(input);
-                break;
-            case 2:
-                cout << q.dequeue() << "\n";
-                break;
-            case 3:
-                cout << q.front() << "\n";
-                break;
-            case 4:
-                cout << q.getSize() << "\n";
-                break;
-            default:
-                cout << ((q.isEmpty()) ? "true\n" : "false\n");
-                break;
-        }
-    }
+    	while (t--) {
+        	int choice, input;
+        	cin >> choice;
+       	 	switch (choice) {
+            		case 1:
+                		cin >> input;
+                		q.enqueue(input);
+                		break;
+            		case 2:
+                		cout << q.dequeue() << "\n";
+                		break;
+            		case 3:
+                		cout << q.front() << "\n";
+                		break;
+            		case 4:
+                		cout << q.getSize() << "\n";
+                		break;
+            		default:
+                		cout << ((q.isEmpty()) ? "true\n" : "false\n");
+                		break;
+        	}
+    	}
 }
