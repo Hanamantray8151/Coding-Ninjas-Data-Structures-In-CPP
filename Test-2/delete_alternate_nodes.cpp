2f200d78ff32c6299030944d39d74950a2b1fbad
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-30 00:03:33 +0530

    Update delete_alternate_nodes.cpp

--- Test-2/delete_alternate_nodes.cpp
+++ Test-2/delete_alternate_nodes.cpp
@@ -34,70 +34,70 @@ using namespace std;
 
 class Node{
 public:
-    int data;
-    Node * next;
-    Node(int data){
-        this -> data = data;
-        this -> next = NULL;
-    }
-    
-    ~Node(){
-        if(next){
-            delete next;
+        int data;
+        Node * next;
+        Node(int data){
+                this -> data = data;
+                this -> next = NULL;
         }
-    }
+    
+        ~Node(){
+                if(next){
+                        delete next;
+                }
+        }
 };
 
 Node* takeinput(){
-    int data;
-    cin >> data;
-    Node *head = NULL, *tail = NULL;
-    while(data != -1){
-        Node *newNode = new Node(data);
-        if(head == NULL){
-            head = newNode;
-            tail = newNode;
-        }else{
-            tail -> next = newNode;
-            tail = newNode;
-        }
+        int data;
         cin >> data;
-    }
-    return head;
+        Node *head = NULL, *tail = NULL;
+        while(data != -1){
+                Node *newNode = new Node(data);
+                if(head == NULL){
+                        head = newNode;
+                        tail = newNode;
+                }else{
+                        tail -> next = newNode;
+                        tail = newNode;
+                }
+                cin >> data;
+        }
+        return head;
 }
 
 void deleteAlternateNodes(Node *head){
-    if(head == NULL){
-        return;
-    }
-    Node *prev = head;
-    Node *curr = head -> next;
-    
-    while(prev != NULL && curr != NULL){
-        prev -> next = curr -> next;
-        
-        curr -> next = NULL;
-        prev = prev -> next;
-        
-        if(prev != NULL){
-            curr = prev -> next;
+        if(head == NULL){
+            return;
         }
-    }
+        Node *prev = head;
+        Node *curr = head -> next;
+    
+        while(prev != NULL && curr != NULL){
+                prev -> next = curr -> next;
+        
+                curr -> next = NULL;
+                prev = prev -> next;
+        
+                if(prev != NULL){
+                        curr = prev -> next;
+                }
+        }
 }
 
 void print(Node *head){
-    Node *temp = head;
-    while(temp != NULL){
-        cout << temp -> data << " ";
-        temp = temp -> next;
-    }
-    cout << endl;
+        Node *temp = head;
+        while(temp != NULL){
+                cout << temp -> data << " ";
+                temp = temp -> next;
+        }
+        cout << endl;
 }
 
 int main(){
-    Node *head = takeinput();
-    deleteAlternateNodes(head);
-    print(head);
-    delete head;
-    return 0;
+        Node *head = takeinput();
+        deleteAlternateNodes(head);
+        print(head);
+        delete head;
+        return 0;
 }
