6e7c380280a92cb893aa738533ae2b3b1ab9fe67
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-07-31 00:58:50 +0530

    Create staircase_using_dp.cpp

---
+++ DP-1/staircase_using_dp.cpp
@@ -0,0 +1,50 @@
+/*
+A child runs up a staircase with 'n' steps and can hop either 1 step, 2 steps or 3 steps at a time. Implement a method 
+to count and return all possible ways in which the child can run-up to the stairs.
+Input format :
+The first and the only line of input contains an integer value, 'n', denoting the total number of steps.
+Output format :
+Print the total possible number of ways.
+Constraints :
+0 <= n <= 10 ^ 4
+
+Time Limit: 1 sec
+Sample Input 1:
+4
+Sample Output 1:
+7
+Sample Input 2:
+10
+Sample Output 2:
+274
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+long helper(int n,int *dp){
+	dp[0] = 1;
+	dp[1] = 1;
+	dp[2] = 2;
+	
+	for(int i=3;i<=n;i++){
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+	return dp[n];
+}
+
+long staircase(int n){
+	int *ans = new int[n + 1];
+	for(int i=0;i<=n;i++){
+		ans[i] = -1;
+	}
+	return helper(n,ans);
+}
+
+int main(){
+	int n;
+	cin >> n;
+	cout << staircase(n) << endl;
+
+	return 0;
+}
