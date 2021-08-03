0f15bf086e0024893ae61a0a26553882a12c8b2b
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-08-04 00:05:12 +0530

    Update dfs.cpp

--- Graphs-1/dfs.cpp
+++ Graphs-1/dfs.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void print(int **edges,int n,int sv,bool *visited){
+void printDFS(int **edges,int n,int sv,bool *visited){
 	cout << sv << endl;
 	visited[sv] = true;
 	for(int i=0;i<n;i++){
@@ -10,41 +10,51 @@ void print(int **edges,int n,int sv,bool *visited){
 				if(visited[i]){
 					continue;
 				}
-				print(edges,n,i,visited);
+				printDFS(edges,n,i,visited);
 			}
 		}
 	}
 }
 
+void DFS(int **edges,int n){
+	bool *visited = new bool(n);
+    	for(int i=0;i<n;i++){
+        	visited[i] = false;
+    	}
+    	for(int i=0;i<n;i++){
+    		if(!visited[i]){
+    			printDFS(edges,n,i,visited);
+    		}
+    	}
+    	delete [] visited;
+}
+
 int main(){
 	int n,e;
-  cin >> n >> e;
+    	cin >> n >> e;
 
-    int **edges = new int*[n];
-    for(int i=0;i<n;i++){
-        edges[i] = new int[n];
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            edges[i][j] = 0;
-        }
-    }
+    	int **edges = new int*[n];
+    	for(int i=0;i<n;i++){
+        	edges[i] = new int[n];
+    	}
+    	for(int i=0;i<n;i++){
+        	for(int j=0;j<n;j++){
+            		edges[i][j] = 0;
+        	}
+    	}
 
-    for(int i=0;i<e;i++){
-        int f,s;
-        cin >> f >> s;
-        edges[f][s] = 1;
-        edges[s][f] = 1;
-    }
+    	for(int i=0;i<e;i++){
+        	int f,s;
+        	cin >> f >> s;
+        	edges[f][s] = 1;
+        	edges[s][f] = 1;
+   	}
 
-    bool *visited = new bool(n);
-    for(int i=0;i<n;i++){
-        visited[i] = false;
-    }
-   	
-    print(edges,n,0,visited);
+    	DFS(edges,n);
     
-    delete [] edges;
-    delete [] visited;
-    return 0;
+    	for(int i=0;i<n;i++){
+    		delete [] edges[i];
+    	}
+    	delete [] edges;
+    	return 0;
 }
