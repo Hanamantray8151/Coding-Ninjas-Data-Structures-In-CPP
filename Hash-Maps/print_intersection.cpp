d67c382b39bb8f9ad0e74bb1dbb5db05a6d91a39
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-08-09 12:20:12 +0530

    Create print_intersection.cpp

---
+++ Hash-Maps/print_intersection.cpp
@@ -0,0 +1,81 @@
+/*
+You have been given two integer arrays/lists (ARR1 and ARR2) of size N and M, respectively. You need to print their 
+intersection; An intersection for this problem can be defined when both the arrays/lists contain a particular value or 
+to put it in other words, when there is a common value that exists in both the arrays/lists.
+Note :
+Input arrays/lists can contain duplicate elements.
+
+The intersection elements printed would be in the order they appear in the first sorted array/list (ARR1).
+
+
+Input format :
+The first line of input contains an integer 'N' representing the size of the first array/list.
+
+The second line contains 'N' single space separated integers representing the elements of the first the array/list.
+
+The third line contains an integer 'M' representing the size of the second array/list.
+
+The fourth line contains 'M' single space separated integers representing the elements of the second array/list.
+Output format :
+Print the intersection elements. Each element is printed in a separate line.
+Constraints :
+0 <= N <= 10^6
+0 <= M <= 10^6
+
+Time Limit: 1 sec 
+Sample Input 1 :
+6
+2 6 8 5 4 3
+4
+2 3 4 7 
+Sample Output 1 :
+2
+3 
+4
+Sample Input 2 :
+4
+2 6 1 2
+5
+1 2 3 4 2
+Sample Output 2 :
+1 
+2 
+2
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void printIntersection(int *a,int *b,int m,int n){
+	unordered_map<int,int> mapuse;
+	for(int i=0;i<m;i++){
+		mapuse[a[i]]++;
+	}
+	
+	for(int i=0;i<n;i++){
+		if(mapuse.count(b[i]) > 0){
+			cout << b[i] << endl;
+		}
+	}
+}
+
+int main(){
+	int n1,n2;
+	cin >> n1;
+	int *a1 = new int[n1];
+	for(int i=0;i<n1;i++){
+		cin >> a1[i];
+	}
+	
+	cin >> n2;
+	int *a2 = new int[n2];
+	for(int i=0;i<n2;i++){
+		cin >> a2[i];
+	}
+	
+	printIntersection(a1,a2,n1,n2);
+
+	delete [] a1;
+	delete [] a2;
+	return 0;
+}
