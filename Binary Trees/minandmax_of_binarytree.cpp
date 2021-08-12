2a99fabdde0a1e305aca85f6b2f3319af4bcf0ce
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-08-12 22:28:11 +0530

    Create minandmax_of_binarytree.cpp

---
+++ Binary Trees/minandmax_of_binarytree.cpp
@@ -0,0 +1,118 @@
+/*
+For a given a Binary Tree of type integer, find and return the minimum and the maximum data values.
+Return the output as an object of Pair class, which is already created.
+Note:
+All the node data will be unique and hence there will always exist a minimum and maximum node data.
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an 
+indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The only line of output prints two integers denoting the minimum and the maximum data values respectively. A single line will
+separate them both.
+Constraints:
+2 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+Sample Output 1:
+1 14
+Sample Input 2:
+10 20 60 -1 -1 3 50 -1 -1 -1 -1 
+Sample Output 2:
+3 60
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class BinaryTreeNode{
+	public:
+		int data;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+		
+		BinaryTreeNode(int data){
+			this -> data = data;
+			this -> left = NULL;
+			this -> right = NULL;
+		}
+		
+		~BinaryTreeNode(){
+			delete left;
+			delete right;
+		}
+};
+
+BinaryTreeNode* takeinputLevelwise(){
+	queue<BinaryTreeNode*> q;
+	int rootData;
+	cin >> rootData;
+	
+	if(rootData == -1){
+		return NULL;
+	}
+	
+	BinaryTreeNode* root = new BinaryTreeNode(rootData);
+	q.push(root);
+	
+	while(!q.empty()){
+		BinaryTreeNode* front = q.front();
+		q.pop();
+		
+		int leftChildData;
+		cin >> leftChildData;
+		if(leftChildData != -1){
+			BinaryTreeNode* child = new BinaryTreeNode(leftChildData);
+			front -> left = child;
+			q.push(child);
+		}
+		
+		int rightChildData;
+		cin >> rightChildData;
+		if(rightChildData != -1){
+			BinaryTreeNode* child = new BinaryTreeNode(rightChildData);
+			front -> right = child;
+			q.push(child);
+		}
+	}
+	return root;
+}
+
+void helper(BinaryTreeNode* root,int &minimum,int &maximum){
+	if(root == NULL){
+		return;
+	}
+	
+	minimum = min(minimum,root -> data);
+	maximum = max(maximum,root -> data);
+	
+	helper(root -> left,minimum,maximum);
+	helper(root -> right,minimum,maximum);
+}
+
+pair<int,int> getMinAndMax(BinaryTreeNode* root){
+	int minimum = INT_MAX;
+	int maximum = INT_MIN;
+	
+	helper(root,minimum,maximum);
+	
+	pair<int,int> p;
+	p.first = minimum;
+	p.second = maximum;
+	
+	return p;
+}
+
+//1 2 3 4 5 -1 -1 6 -1 -1 7 8 -1 -1 9 -1 -1 -1 -1
+
+int main(){
+	BinaryTreeNode* root = takeinputLevelwise();
+	
+	pair<int,int> p = getMinAndMax(root);
+	cout << "Minimum : " << p.first << endl;
+	cout << "Maximum : " << p.second << endl;
+
+	return 0;
+}
