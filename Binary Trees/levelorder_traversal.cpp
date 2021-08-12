8560495b4d54adc133f2f14ef313964cdeb7562c
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-08-13 00:23:26 +0530

    Create levelorder_traversal.cpp

---
+++ Binary Trees/levelorder_traversal.cpp
@@ -0,0 +1,125 @@
+/*
+For a given a Binary Tree of type integer, print it in a level order fashion where each level will be printed on a new line. 
+Elements on every level will be printed in a linear fashion and a single space will separate them.
+
+Input Format:
+The first and the only line of input will contain the node data, all separated by a single space. Since -1 is used as an indication whether the left or right node data exist for root, it will not be a part of the node data.
+Output Format:
+The given input tree will be printed in a level order fashion where each level will be printed on a new line. 
+Elements on every level will be printed in a linear fashion. A single space will separate them.
+Constraints:
+1 <= N <= 10^5
+Where N is the total number of nodes in the binary tree.
+
+Time Limit: 1 sec
+Sample Input 1:
+10 20 30 40 50 -1 60 -1 -1 -1 -1 -1 -1 
+Sample Output 1:
+10 
+20 30 
+40 50 60 
+Sample Input 2:
+8 3 10 1 6 -1 14 -1 -1 4 7 13 -1 -1 -1 -1 -1 -1 -1
+Sample Output 2:
+8 
+3 10 
+1 6 14 
+4 7 13 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class BinaryTreeNode{
+	public:
+		int data;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+		
+		BinaryTreeNode(int data){
+			this -> data = data;
+			this -> left = NULL;
+			this -> right = NULL;
+		}
+		
+		~BinaryTreeNode(){
+			delete left;
+			delete right;
+		}
+};
+
+BinaryTreeNode* takeinputLevelwise(){
+	queue<BinaryTreeNode*> q;
+	int rootData;
+	cin >> rootData;
+	
+	if(rootData == -1){
+		return NULL;
+	}
+	
+	BinaryTreeNode* root = new BinaryTreeNode(rootData);
+	q.push(root);
+	
+	while(!q.empty()){
+		BinaryTreeNode* front = q.front();
+		q.pop();
+		
+		int leftChildData;
+		cin >> leftChildData;
+		if(leftChildData != -1){
+			BinaryTreeNode* child = new BinaryTreeNode(leftChildData);
+			front -> left = child;
+			q.push(child);
+		}
+		
+		int rightChildData;
+		cin >> rightChildData;
+		if(rightChildData != -1){
+			BinaryTreeNode* child = new BinaryTreeNode(rightChildData);
+			front -> right = child;
+			q.push(child);
+		}
+	}
+	return root;
+}
+
+void printLevelWise(BinaryTreeNode* root){
+	queue<BinaryTreeNode*> q;
+	if(root == NULL){
+		return;
+	}
+	q.push(root);
+	q.push(NULL);
+	
+	while(!q.empty()){
+		BinaryTreeNode* front = q.front();
+		q.pop();
+		if(front == NULL){
+			if(q.empty()){
+				return;
+			}else{
+				cout << endl;
+				q.push(NULL);
+			}
+		}else{
+			cout << front -> data << " ";
+			if(front -> left != NULL){
+				q.push(front -> left);
+			}
+			if(front -> right != NULL){
+				q.push(front -> right);
+			}
+		}
+	}
+}
+
+//1 2 3 4 5 -1 -1 6 -1 -1 7 8 -1 -1 9 -1 -1 -1 -1
+
+int main(){
+	BinaryTreeNode* root = takeinputLevelwise();
+	
+	printLevelWise(root);
+	cout << endl;
+	
+	return 0;
+}
