38944f23706df5d22ba60d27b5f1e960a5c4e587
Hanamantray Kumbar <68737652+Hanamantray8151@users.noreply.github.com>
2021-08-26 11:19:47 +0530

    Create find_dupplicate_number.cpp

---
+++ LeetCode(SDE Cheet Sheet)/find_dupplicate_number.cpp
@@ -0,0 +1,30 @@
+//O(n * log n).
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == nums[i + 1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
+
+//O(n).
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mapuse;
+        for(int i=0;i<nums.size();i++){
+            mapuse[nums[i]]++;
+        }
+        for(auto m : mapuse){
+            if(m.second > 1){
+                return m.first;
+            }
+        }
+        return -1;
+    }
+};
